828f0ae02743272a54cae980e59cdfb9e3cd3128
aradley <arthur_radley@hotmail.co.uk>
2016-02-17 15:24:27 -0500

    Update make_cellnet.R

--- make_cellnet.R
+++ make_cellnet.R
@@ -7,6 +7,8 @@ cn_make_processor<-function # train a CellNet object
 (expTrain,
  stTrain,
  ctGRNs, # result of running cn_grnDoRock
+ expVal, # Data for Classifier validation
+ stVal, # Data for Classifier validation
  tctOrder=NULL,
  dLevel="description1",
  classWeight=TRUE, # weight GRN est by classification importance
@@ -24,6 +26,10 @@ cn_make_processor<-function # train a CellNet object
   trainNorm<-cn_trainNorm(expTrain, stTrain, subNets=geneLists, classList=classList, dLevel=dLevel, classWeight=classWeight, exprWeight=exprWeight);
   meanExpTrain<-as.matrix(GEP_makeMean(expTrain,as.vector(stTrain[,dLevel]),type='mean'));
   
+  #Make Object for Classifier Assessment
+  ansVal<-cn_classify(classList, expVal, ctGRNs[['ctGRNs']][['geneLists']]) 
+  assessed<-cn_classAssess(ansVal, stVal, classLevels="description1", resolution=0.01) 
+  
   ans<-list(expTrain=expTrain,
             expMeanTrain=meanExpTrain,
             stTrain=stTrain,
@@ -37,7 +43,8 @@ cn_make_processor<-function # train a CellNet object
             minVals=trainNorm[['minVals']],
             tVals=trainNorm[['tVals']],
             classWeight=classWeight,
-            exprWeight=exprWeight);
+            exprWeight=exprWeight,
+            ClassifierAssessment = assessed);
   class(ans)<-"cnProc";
   ans;
 }
@@ -53,4 +60,5 @@ make_classifiers<-function# wrappper to cn_makeRFs
 	geneLists<-ctGRNs[['ctGRNs']][['grnLists']];
  	cn_makeRFs(expDat,sampTab,geneLists, dLevel=dLevel);
 }
+
 
